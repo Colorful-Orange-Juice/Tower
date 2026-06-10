7b56c30a5247bc69a347c81dffd5e634e45e48c4
Connor-Villanueva <ckvillan@calpoly.edu>
2026-06-09 21:09:36 -0700

    Testing player spawning

--- scenes/tests/test1_attack/test_scene.gd
+++ scenes/tests/test1_attack/test_scene.gd
@@ -6,20 +6,20 @@ const PLAYER_SCENE = preload("res://scenes/player_attack/Player.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	for player in multiplayer.get_peers():
-		spawn_player(player)
+	if (not multiplayer.is_server()):
+		return
+	
+	spawn_player(1) # Spawn host
+	
+	for peer_id in multiplayer.get_peers():
+		print("Spawning peer: ", peer_id)
+		spawn_player(peer_id)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
-
-##
-## Spawns a character for the incoming client
-##
-func _on_player_connected(id : int) -> void:
-	spawn_player(id)
 
 ##
 ## Spawns a player, assigning an id
@@ -33,5 +33,7 @@ func spawn_player(id : int) -> void:
 	# Spawner will replicate this player to all clients automatically
 	players_container.add_child(player)
 	
+	await get_tree().process_frame # Wait a frame before making rpc call
+	
 	# Tell the server and all clients to move the player
-	player.set_initial_position(spawn_marker.global_position)
+	player.set_initial_position.rpc(spawn_marker.global_position)
